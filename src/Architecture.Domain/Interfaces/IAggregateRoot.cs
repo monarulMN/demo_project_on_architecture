0af859686f48edbfa107cf714449d7b13ad9eab5
monarulMN <mdmonarulislam720@gmail.com>
2026-06-26 21:03:03 +0600

    Architect code added

---
+++ src/Architecture.Domain/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Architecture.Domain.Interfaces
+{
+    public interface IAggregateRoot<TKey>
+    {
+        TKey Id { get; set; }
+    }
+}
---
+++ src/Architecture.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Architecture.Domain.Interfaces
+{
+    public interface IRepository<TAggregateRoot, TKey>
+        where TAggregateRoot : class, IAggregateRoot<TKey>
+        where TKey : IComparable
+    {
+        void Add(TAggregateRoot entity);
+        void AddAsync(TAggregateRoot entity);
+        void Update(TAggregateRoot entity);
+        void UpdateAsync(TAggregateRoot entity);
+        void Delete(TAggregateRoot entity);
+        void DeleteAsync(TAggregateRoot entity);
+    }
+}
