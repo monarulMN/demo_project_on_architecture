66cc00ead8cf57436b9aee0bb17606457c39bf73
monarulMN <mdmonarulislam720@gmail.com>
2026-06-29 18:40:35 +0600

    Create entity, migration & hard code value insert

--- src/Architecture.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Architecture.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Architecture.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Architecture.Infrastructure.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ src/Architecture.Web/Controllers/TeamController.cs
@@ -0,0 +1,24 @@
+﻿using Architecture.Application.Contracts;
+using Architecture.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Architecture.Web.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly IApplicationUnitOfWork _unitOfWork;
+
+        public TeamController(IApplicationUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            _unitOfWork.ProductRepository.Add(new Product { Id = Guid.NewGuid(), Name = " Sample Product", Price = 99});
+            _unitOfWork.Save();
+
+            return View();
+        }
+    }
+}
